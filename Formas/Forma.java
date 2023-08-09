8ccaf5b41d4f44013befc29b5564ee1e8b28f161
Moisesl2 <moiseshenrique080@gmail.com>
2023-08-09 20:09:04 -0300

    first commit

---
+++ Formas/Forma.java
@@ -0,0 +1,17 @@
+package Formas;
+
+public class Forma {
+    private double area;
+
+    public Forma(){
+
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+}
---
+++ Formas/FormaTri.java
@@ -0,0 +1,16 @@
+package Formas;
+
+public class FormaTri extends Forma{
+   private double volume;
+
+    public FormaTri(){
+
+    }
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    } 
+}
